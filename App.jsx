21abb6f30868cc97545e1a7991fc584f60a2c2cf
kelp-of-truth <newkonbu0918@gmail.com>
2025-09-08 20:46:51 +0900

    Updates

---
+++ App.jsx
@@ -0,0 +1,13 @@
+// import { BrowserRouter, Link, Switch, Route } from "react-router-dom"
+// import  Home  from "./components/Home"
+// import Page1 from './components/Page1'
+
+function App() {
+  return (
+    <div className="App">
+      hoge
+    </div>
+  )
+}
+
+export default App
